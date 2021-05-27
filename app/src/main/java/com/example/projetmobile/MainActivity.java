051a8006345cd4fc10ee828440f6e4c8933eccd0
triffort <thibaut.riffort@wanadoo.fr>
2021-05-27 18:09:05 +0200

    Ajout de l'appel serveur

--- app/src/main/java/com/example/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/projetmobile/MainActivity.java
@@ -10,10 +10,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BaseActivity implements MainInterface{
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final String BASE_URL = "https://pokeapi.co/";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -24,6 +35,46 @@ public class MainActivity extends BaseActivity implements MainInterface{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        showList();
+        makeApiCall();
+
+    }
+
+
+
+        private void makeApiCall(){
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+
+            PokeApi pokeApi = retrofit.create(PokeApi.class);
+
+            Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
+            call.enqueue(new Callback<RestPokemonResponse>() {
+                @Override
+                public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                    if(response.isSuccessful() && response.body() != null){
+                        List<Pokemon> pokemonList = response.body().getResults();
+                        Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                    } else {
+                        showError();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                        showError();
+                }
+            });
+        };
+
+
+    public void showList() {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
@@ -39,34 +90,13 @@ public class MainActivity extends BaseActivity implements MainInterface{
         // define an adapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
-
-
-        Button mainButton = findViewById(R.id.button);
-        mainButton.setBackgroundColor(Color.RED);
-        mainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Onclicked", Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-        Coureur coureur = new Coureur("julian", 28);
-        showBaseError();
     }
-
-    @Override
-    public void showList(List<Coureur> list) {
-
-    }
-
-    @Override
+    
     public void showLoader() {
 
     }
 
-    @Override
-    public void showError() {
-
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/projetmobile/MainInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.projetmobile;
-
-import java.util.List;
-
-public interface MainInterface {
-    void showList(List<Coureur> list);
-    void showLoader();
-    void showError();
-}
