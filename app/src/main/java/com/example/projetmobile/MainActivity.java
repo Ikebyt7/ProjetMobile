5b2bfad562e8b00bb9a19271c761ca58143e6249
triffort <thibaut.riffort@wanadoo.fr>
2021-05-27 17:12:28 +0200

    Ajout de la liste

--- app/src/main/java/com/example/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/projetmobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projetmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -8,14 +10,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity implements MainInterface{
+
+    private RecyclerView recyclerView;
+    private ListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }
+
+        // define an adapter
+        mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+
 
         Button mainButton = findViewById(R.id.button);
         mainButton.setBackgroundColor(Color.RED);
