ce82d10831b5035533850db8d3ef5af699a96dbc
Ikebyt7 <thibaut.riffort@wanadoo.fr>
2021-05-28 11:39:16 +0200

    Stock en cache

---
+++ app/src/main/java/com/example/projetmobile/Constants.java
@@ -0,0 +1,5 @@
+package com.example.projetmobile;
+
+public class Constants {
+    static String KEY_COUREUR_LIST = "jsonCoureurList";
+}
--- app/src/main/java/com/example/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/projetmobile/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,14 +30,41 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeApiCall();
+        sharedPreferences = getSharedPreferences("appli", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
 
+        List<Coureur> coureurList = getDataFromCache();
+
+        if (coureurList != null) {
+            showList(coureurList);
+        } else {
+            makeApiCall();
+        }
+
+
+    }
+
+    private List<Coureur> getDataFromCache() {
+
+        String jsonCoureur = sharedPreferences.getString(Constants.KEY_COUREUR_LIST, null);
+
+        if (jsonCoureur == null) {
+            return null;
+        } else {
+            Type listType = new TypeToken<List<Coureur>>() {
+            }.getType();
+            return gson.fromJson(jsonCoureur, listType);
+        }
     }
 
     public void showList(List<Coureur> coureurList) {
@@ -47,9 +78,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeApiCall() {
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -64,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<RestCoureurResponse> call, Response<RestCoureurResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     List<Coureur> coureurList = response.body().getResults();
+                    saveList(coureurList);
                     showList(coureurList);
                 } else {
                     showError();
@@ -75,6 +104,17 @@ public class MainActivity extends AppCompatActivity {
                 showError();
             }
         });
+    }
+
+    private void saveList(List<Coureur> coureurList) {
+        String jsonString = gson.toJson(coureurList);
+
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_COUREUR_LIST, jsonString)
+                .apply();
+
+        Toast.makeText(getApplicationContext(), "Liste saved", Toast.LENGTH_SHORT).show();
     }
 
     ;
